7dd1e79fa7fa779f5bc6d7a477543b5644e8fe2c
GenarC <cemgenar43@gmail.com>
2018-02-04 17:46:10 +0300

    S07.01_Addition

--- S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
+++ S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
@@ -15,6 +15,8 @@
  */
 package com.example.android.sunshine.data;
 
+import android.provider.BaseColumns;
+
 /**
  * Defines table and column names for the weather database. This class is not necessary, but keeps
  * the code organized.
